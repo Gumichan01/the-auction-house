26b5266a2d718992415b611aa90d88cc9bafffab
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-13 19:13:28 +0200

    Set hashCode() and equals() for AuctionHouse

--- src/main/java/com/gumichan01/challenge/domain/AuctionHouse.java
+++ src/main/java/com/gumichan01/challenge/domain/AuctionHouse.java
@@ -3,6 +3,7 @@ package com.gumichan01.challenge.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class AuctionHouse {
@@ -29,6 +30,23 @@ public class AuctionHouse {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (!(o instanceof AuctionHouse))
+            return false;
+
+        AuctionHouse that = (AuctionHouse) o;
+        return id.equals(that.id) && name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     @Override
