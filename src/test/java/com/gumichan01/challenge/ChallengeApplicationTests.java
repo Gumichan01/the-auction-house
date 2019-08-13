fd6257eb732812d4f5b7444fb05bd5d3a92c22c4
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-13 20:43:33 +0200

    Add new test - add new auction house

--- src/test/java/com/gumichan01/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/gumichan01/challenge/ChallengeApplicationTests.java
@@ -1,14 +1,20 @@
 package com.gumichan01.challenge;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gumichan01.challenge.domain.AuctionHouse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,5 +33,19 @@ public class ChallengeApplicationTests {
         String expectedJsonContent = "[]";
         this.mockMvc.perform(get(url)).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().json(expectedJsonContent));
+    }
+
+    @Test
+    public void addAuctionHouseByProvidingItsNameAndReturnTheObjectInJsonFormat() throws Exception {
+        String url = "/houses";
+        String expectedJsonContent = "{\"id\":1,\"name\"=\"mock house\"}";
+        String jsonRequestContent = jsonOf(new AuctionHouse("mock house"));
+        this.mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON_UTF8).content(jsonRequestContent))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(expectedJsonContent));
+    }
+
+    private String jsonOf(@NonNull Object object) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(object);
     }
 }
