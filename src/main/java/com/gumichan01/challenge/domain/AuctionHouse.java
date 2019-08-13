66f851a8ba0a3762a950de00413762a1842ceced
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-13 16:40:33 +0200

    Define an auction house in the domain

---
+++ src/main/java/com/gumichan01/challenge/domain/AuctionHouse.java
@@ -0,0 +1,31 @@
+package com.gumichan01.challenge.domain;
+
+public class AuctionHouse {
+
+    private Long id = 1L; // Just to simplify
+    private String name;
+
+    public AuctionHouse(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "AuctionHouse{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
