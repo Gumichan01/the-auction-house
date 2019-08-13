3df2aa4ca162f3f7c69298fc21ad89791d123e6c
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-13 18:49:32 +0200

    Remove the data preload

--- src/main/java/com/gumichan01/challenge/ChallengeApplication.java
+++ src/main/java/com/gumichan01/challenge/ChallengeApplication.java
@@ -1,11 +1,7 @@
 package com.gumichan01.challenge;
 
-import com.gumichan01.challenge.domain.AuctionHouse;
-import com.gumichan01.challenge.persistence.AuctionHouseRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -13,15 +9,5 @@ public class ChallengeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChallengeApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner initDatabase(AuctionHouseRepository repository) {
-        return args -> {
-
-            System.out.println("Preloading " + repository.save(new AuctionHouse("miku house")));
-            System.out.println("Preloading " + repository.save(new AuctionHouse("gumi house")));
-            System.out.println("Preloading " + repository.save(new AuctionHouse("luka house")));
-        };
     }
 }
