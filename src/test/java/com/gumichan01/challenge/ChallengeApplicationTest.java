dddcd7d40186f10a3be06b3718fba6182d88fcb1
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-14 16:02:57 +0200

    Rename a test class

--- src/test/java/com/gumichan01/challenge/ChallengeApplicationTest.java
+++ src/test/java/com/gumichan01/challenge/ChallengeApplicationTest.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class ChallengeApplicationTests {
+public class ChallengeApplicationTest {
 
     @Autowired
     private MockMvc mockMvc;
