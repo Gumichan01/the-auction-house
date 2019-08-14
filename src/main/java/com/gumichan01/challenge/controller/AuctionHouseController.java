8798176f6e7da27aab13dc810a6250d382ec896e
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-14 23:13:53 +0200

    Handle more exceptions in controller

--- src/main/java/com/gumichan01/challenge/controller/AuctionHouseController.java
+++ src/main/java/com/gumichan01/challenge/controller/AuctionHouseController.java
@@ -3,6 +3,8 @@ package com.gumichan01.challenge.controller;
 import com.gumichan01.challenge.domain.AuctionHouse;
 import com.gumichan01.challenge.service.AuctionHouseService;
 import com.gumichan01.challenge.service.exception.AlreadyRegisteredException;
+import com.gumichan01.challenge.service.exception.BadRequestException;
+import com.gumichan01.challenge.service.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,5 +43,15 @@ public class AuctionHouseController {
     @ExceptionHandler(AlreadyRegisteredException.class)
     public ResponseEntity<String> handleError(AlreadyRegisteredException e) {
         return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequest(BadRequestException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleNoResourceFound(ResourceNotFoundException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
