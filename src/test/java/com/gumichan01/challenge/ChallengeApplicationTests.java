ab4c76d15d158b22b3481afd3720b53703eaf4df
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-14 14:50:10 +0200

    Reload the application context after each test
    A test fail because of the side effect on the application context.
    That is why I force the context reset.

--- src/test/java/com/gumichan01/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/gumichan01/challenge/ChallengeApplicationTests.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class ChallengeApplicationTests {
 
     @Autowired
