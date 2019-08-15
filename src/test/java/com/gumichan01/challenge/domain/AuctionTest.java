7f9dd5b6ed7764e4652fa3b521aa022d0cdc391f
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-15 19:33:41 +0200

    Define a basic auction test

---
+++ src/test/java/com/gumichan01/challenge/domain/AuctionTest.java
@@ -0,0 +1,24 @@
+package com.gumichan01.challenge.domain;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AuctionTest {
+
+    @Test
+    public void shouldHaveTheCurrentPriceEqualToTheStartPrice() {
+
+        Calendar instance = Calendar.getInstance();
+        Date startTime = instance.getTime();
+        instance.add(Calendar.DAY_OF_MONTH,1 );
+        Date endTime = instance.getTime();
+        AuctionHouse auctionHouse = new AuctionHouse("gumi house");
+        double startPrice = 1.0;
+        Auction auction = new Auction("hello", "lorem ipsum",startTime, endTime, startPrice, auctionHouse);
+        assertThat(auction.getCurrentPrice()).isEqualTo(startPrice);
+    }
+}
