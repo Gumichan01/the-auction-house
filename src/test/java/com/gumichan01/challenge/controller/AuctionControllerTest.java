e0136053facde79c34ce10ec06a81647cd233b0f
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-15 22:29:42 +0200

    Add another test in the auction controller

--- src/test/java/com/gumichan01/challenge/controller/AuctionControllerTest.java
+++ src/test/java/com/gumichan01/challenge/controller/AuctionControllerTest.java
@@ -1,5 +1,10 @@
 package com.gumichan01.challenge.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gumichan01.challenge.controller.dto.AuctionDto;
+import com.gumichan01.challenge.domain.Auction;
+import com.gumichan01.challenge.domain.AuctionHouse;
 import com.gumichan01.challenge.service.AuctionService;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,8 +13,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.lang.NonNull;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -30,7 +41,7 @@ public class AuctionControllerTest {
     private MockMvc mockMvc;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
         MockitoAnnotations.initMocks(this);
         mockMvc = standaloneSetup(controller).build();
@@ -44,4 +55,24 @@ public class AuctionControllerTest {
                 .andExpect(content().json(expectedJsonContent));
     }
 
+    @Test
+    public void shouldReturnNonEmptyListAndSendHttpOk() throws Exception {
+        String AUCTION_TO_HOUSE_BY_ID_URL = AUCTION_URL + "/42";
+        Calendar instance = Calendar.getInstance();
+        Date startTime = instance.getTime();
+        instance.add(Calendar.DAY_OF_MONTH, 1);
+        Date endTime = instance.getTime();
+        AuctionHouse auctionHouse = new AuctionHouse("gumi house");
+        double startPrice = 42.0;
+
+        Auction auction = new Auction("gumi auction", "lorem ipsum", startTime, endTime, startPrice, auctionHouse);
+        String expectedJsonContent = "[" + jsonOf(new AuctionDto(auction)) + "]";     // We expect an JSON array, not a JSON object
+        when(service.retieveAuctions(42L)).thenReturn(Arrays.asList(auction));
+        this.mockMvc.perform(get(AUCTION_TO_HOUSE_BY_ID_URL)).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(expectedJsonContent));
+    }
+
+    private String jsonOf(@NonNull Object object) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(object);
+    }
 }
