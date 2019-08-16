c7250f86dec26da2657b5e8ada8dc830ee2a00f0
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-16 17:21:50 +0200

    Add auction - basic implementation

--- src/main/java/com/gumichan01/challenge/controller/AuctionController.java
+++ src/main/java/com/gumichan01/challenge/controller/AuctionController.java
@@ -11,10 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,6 +33,11 @@ public class AuctionController {
     public List<AuctionDto> retrieveAuctions(@PathVariable("house_id") Long houseId) {
         List<Auction> auctions = service.retrieveAuctionsBy(houseId);
         return generateDto(auctions);
+    }
+
+    @PostMapping("/houses/auctions/")
+    public AuctionDto registerAuction(@RequestBody AuctionDto auctionDto) {
+        return auctionDto;
     }
 
     private List<AuctionDto> generateDto(List<Auction> auctions) {
