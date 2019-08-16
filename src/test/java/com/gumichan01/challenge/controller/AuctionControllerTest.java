607769eafa0a035a89441942e783ce4c0e3bfde0
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-16 23:23:27 +0200

    Remove the auction registration flaky test.
    Flaky tests are worse than no test.

--- src/test/java/com/gumichan01/challenge/controller/AuctionControllerTest.java
+++ src/test/java/com/gumichan01/challenge/controller/AuctionControllerTest.java
@@ -2,7 +2,6 @@ package com.gumichan01.challenge.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gumichan01.challenge.controller.dto.AuctionDto;
 import com.gumichan01.challenge.service.AuctionService;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,14 +10,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Calendar;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -54,15 +49,6 @@ public class AuctionControllerTest {
     @Test
     public void shouldReturnBadRequestStatusWhenAnInvalidIdIsProvided() throws Exception {
         this.mockMvc.perform(get(AUCTION_URL + "/null")).andDo(print()).andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void shouldReturnCreatedStatusWithTheRegisteredAuction() throws Exception {
-        AuctionDto dto = new AuctionDto("foo", "lorem", Calendar.getInstance().getTime(),
-                Calendar.getInstance().getTime(), 42.0, 42.0, 1L);
-        String jsonContent = jsonOf(dto);
-        this.mockMvc.perform(post(AUCTION_URL + "/").contentType(MediaType.APPLICATION_JSON_UTF8)
-                .content(jsonContent)).andDo(print()).andExpect(status().isCreated()); // This test is just temporary
     }
 
     private String jsonOf(@NonNull Object object) throws JsonProcessingException {
