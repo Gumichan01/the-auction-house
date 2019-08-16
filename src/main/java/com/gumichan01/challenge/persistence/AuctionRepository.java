5c457e98cbb77371031896ab2af13b6514447384
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-16 16:48:34 +0200

    Set a custom query in the auction repository

--- src/main/java/com/gumichan01/challenge/persistence/AuctionRepository.java
+++ src/main/java/com/gumichan01/challenge/persistence/AuctionRepository.java
@@ -1,12 +1,15 @@
 package com.gumichan01.challenge.persistence;
 
 import com.gumichan01.challenge.domain.Auction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AuctionRepository extends CrudRepository<Auction, Long> {
 
-    // TODO Define custom methods to get auctions by house Id
     public List<Auction> findAll();
+
+    @Query("SELECT a FROM Auction a WHERE a.auctionHouse.id = ?1")
+    public List<Auction> findAllByHouseId(Long houseId);
 }
