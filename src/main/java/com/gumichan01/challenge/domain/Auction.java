e25da09efee4c4f0467921e5d77f45d59079d3fe
Gumichan01 <luxon.jean.pierre@gmail.com>
2019-08-17 16:03:50 +0200

    Use the IDENTITY generation type for every ids

--- src/main/java/com/gumichan01/challenge/domain/Auction.java
+++ src/main/java/com/gumichan01/challenge/domain/Auction.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 public class Auction {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/com/gumichan01/challenge/domain/AuctionHouse.java
+++ src/main/java/com/gumichan01/challenge/domain/AuctionHouse.java
@@ -1,16 +1,13 @@
 package com.gumichan01.challenge.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 public class AuctionHouse {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String name;
